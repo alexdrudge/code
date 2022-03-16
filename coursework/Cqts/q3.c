73752a3c3ab10f0206a12ed6a395dc36ce0bb764
Alex Rudge <alexdrudge@gmail.com>
2022-03-16 03:11:14 +0000

    q3finished

--- coursework/Cqts/q3.c
+++ coursework/Cqts/q3.c
@@ -7,51 +7,96 @@
 
 void redact_words(const char *text_filename, const char *redact_words_filename){
     // open the files
-    FILE *input, *redact;
+    FILE *input, *redact, *output;
+    output = fopen("result.txt", "w");
+    if (output == NULL) return;
     input = fopen(text_filename, "r");
+    if (input == NULL){
+        fclose(output);
+        return;
+    }
     redact = fopen(redact_words_filename, "r");
-    if (input == NULL) return;
-    if (redact == NULL) return;
+    if (redact == NULL){
+        fclose(output);
+        fclose(input);
+        return;
+    }
 
     // get the length and text from input file
     fseek(input, 0, SEEK_END);
     int input_length = ftell(input);
     fseek(input, 0, SEEK_SET);
     char input_text[input_length+1];
-    if (input_length == 0) return;
+    if (input_length == 0){
+        fclose(input);
+        fclose(redact);
+        fclose(output);
+        return;
+    }
     int input_read_length = fread(input_text, 1, input_length, input);
-    input_text[input_read_length] = '\0'; // should be input_length
+    input_text[input_read_length] = '\0';
+    // create a copy with only lowercase
+    char input_text_lower[input_length+1];
+    for (int i=0;i<input_length+1;i++) input_text_lower[i] = tolower(input_text[i]);
 
     // get the length and text from redact file
     fseek(redact, 0, SEEK_END);
     int redact_length = ftell(redact);
     fseek(redact, 0, SEEK_SET);
     char redact_text[redact_length + 1];
-    if (redact_length == 0) return; // create the empty file before this
+    if (redact_length == 0){
+        fputs(input_text, output); // EOF would be a return anyway
+        fclose(input);
+        fclose(redact);
+        fclose(output);
+        return;
+    }
     int redact_read_length = fread(redact_text, 1, redact_length, redact);
-    redact_text[redact_read_length] = '\0'; // should be redact_length
+    redact_text[redact_read_length] = '\0';
+    // remove spaces and \n from the redact words
+    for (int i=0;i<redact_length;i++){
+        if (redact_text[i] == ' '){
+            for (int j=i;j<redact_length;j++) redact_text[j] = redact_text[j+1];
+            redact_length--;
+            i--;
+        } else if (redact_text[i] == '\n'){
+            redact_text[i] = ',';
+        }
+    }
 
-    // get each item in the list from redact
-    // need to remove any spaces that occur
-    // also make sure not to redact any new lines
-    // something to do with strchr to find where the strings occur
-    char redact_text_copy[redact_length + 1];
-    strcpy(redact_text_copy, redact_text);
-    char *tok_ptr = strtok(redact_text_copy, ",");
+    // find any replace the occurances
+    // strtok seperates the words in the redact file
+    // strstr finds where these words appear in the input
+    char *tok_ptr = strtok(redact_text, ",");
     while (tok_ptr != NULL) {
-        printf("%s\n", tok_ptr);
+        int tok_length = strlen(tok_ptr);
+        char *inp_ptr = strstr(input_text_lower, tok_ptr);
+        while (inp_ptr != NULL){
+            if (!isalnum(inp_ptr[tok_length]) && (!isalnum(inp_ptr[-1]) || inp_ptr == input_text_lower)){
+                for (int i=0;i<tok_length;i++){
+                    inp_ptr[i] = '*';
+                }
+            }
+            inp_ptr = strstr(inp_ptr+1, tok_ptr);
+        }
         tok_ptr = strtok(NULL, ",");
     }
-    printf("%s\n", redact_text);
+
+    // apply the stars to the non-lowered input
+    for (int i=0;i<input_length+1;i++){
+        if (input_text_lower[i] == '*') input_text[i] = '*';
+    }
+    fputs(input_text, output);
 
     // close the files
     fclose(input);
     fclose(redact);
+    fclose(output);
 }
 
 int main(void){
     const char *input_file = "input.txt";
-    const char *redact_file = "redact.txt";
+    const char *redact_file = "words.txt";
     redact_words(input_file, redact_file);
     printf("sucess");
     return 0;
--- coursework/Cqts/test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-    int n = 1200 % 400;
-    int m = 1201 % 400;
-    printf("%d %d", n, m);
-}
